7dac20d2c4f029f765e9333d665d540b318639b3
Andrey Kudryavtsev <lomastering@gmail.com>
2021-03-11 18:04:18 +0300

    fix: donecard

--- src/components/DoneCard/DoneCard.tsx
+++ src/components/DoneCard/DoneCard.tsx
@@ -1,28 +1,15 @@
-import {
-  FC, useCallback, useEffect, useState,
-} from 'react';
+import { FC } from 'react';
 import KanbanCard from '../KanbanCard/KanbanCard';
 import { CardProps } from '../../models/cardProps.model';
 import hourPrice from '../../constants/hourPrice';
 
 const DoneCard: FC<CardProps> = ({ text, date, finished }: CardProps) => {
-  const [price, setPrice] = useState<string>('');
+  const startSeconds = new Date(date!).getTime();
+  const finishSeconds = new Date(finished!).getTime();
+  const hours: number = (finishSeconds - startSeconds) / 1000 / 60 / 60;
+  const total: string = `$${(hourPrice * hours).toFixed(2)}`;
 
-  const calculatePrice = useCallback(() => {
-    const startSeconds = new Date(date!).getTime();
-    const finishSeconds = new Date(finished!).getTime();
-    const hours: number = (finishSeconds - startSeconds) / 1000 / 60 / 60;
-    const total: string = `$${(hourPrice * hours).toFixed(2)}`;
-    setPrice(total);
-  }, [date, finished]);
-
-  useEffect(() => {
-    calculatePrice();
-  }, [calculatePrice]);
-
-  return (
-    <KanbanCard text={text} info={price} />
-  );
+  return <KanbanCard text={text} info={total} />;
 };
 
 export default DoneCard;
